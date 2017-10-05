4ce569fca0f9f54923c9119debd399461a5513bd
Pye-Casey <31807991+Pye-Casey@users.noreply.github.com>
2017-10-05 09:02:37 -0600

    Update parse.js

--- parse.js
+++ parse.js
@@ -70,13 +70,13 @@ function requestPet() {
 	};
 	myRequest.open("GET", "pet.txt", true);
 	myRequest.send(); 
-	
+	console.log("Name: " + petObj.name();
 	// display object
 	var myLocation = document.getElementById("area3");
-	myLocation.innerHTML = "Name: " + petObj.name + "\n";
+	/*myLocation.innerHTML = "Name: " + petObj.name + "\n";
 	myLocation.innerHTML += "Species: " + petObj.species + "\n";
 	myLocation.innerHTML += "Breed: " + petObj.breed + "\n";
-	myLocation.innerHTML += "Temperment: " + petObj.temperment + "\n"; 
+	myLocation.innerHTML += "Temperment: " + petObj.temperment + "\n"; */
 }
 
 /***************************************
